5948dc19656e48645e6337b7010c8d9bbb3b90b8
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-08-29 18:54:44 -0500

    Hello World!

--- file.py
+++ file.py
@@ -84,3 +84,23 @@ Bonus section
 #   print(boolean)         indent
 # fruit.append('raspberry') not a list
 # fruit.pop(1)   not a list
+
+
+name = "Zen"
+print("My name is", 5)
+
+name = "Zen"
+print("My name is " + 5)
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
